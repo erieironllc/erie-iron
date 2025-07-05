04c7f8ffa47893cc2f2ddd0ed43b90d612b76812
jjekkolab <jj@ekkolab.ai>
2025-07-04 19:22:56 -0500

    move view_utils to common

--- erieiron_common/apps.py
+++ erieiron_common/apps.py
@@ -1,6 +1,32 @@
 # erieiron_common/apps.py
+import os
+import sys
+import time
+
 from django.apps import AppConfig
+
+from erieiron_common import settings_common
 
 
 class ErieironCommonConfig(AppConfig):
     name = 'erieiron_common'
+
+    def ready(self):
+        if 'runserver' in sys.argv:
+            import threading
+
+            def init_pubsub():
+                time.sleep(5)  # let system warm up before handling messages
+                if settings_common.START_MESSAGE_QUEUE_PROCESSOR:
+                    from erieiron_common.message_queue.pubsub_manager import init_pubsub_from_cmd_options
+                    pubsub_manager = init_pubsub_from_cmd_options()
+                    print(f"Starting webservice with a pubsub listener {pubsub_manager.get_handler()}")
+                else:
+                    from erieiron_common.message_queue.pubsub_manager import PubSubManager
+                    from erieiron_common import common
+                    PubSubManager.get_instance().initialize(
+                        env=settings_common.MESSAGE_QUEUE_ENV or common.get_machine_name()
+                    )
+                    print(f"Starting webservice pid={os.getpid()}.  Messages will be published to {PubSubManager.get_instance().get_environment()}")
+
+            threading.Thread(target=init_pubsub).start()
